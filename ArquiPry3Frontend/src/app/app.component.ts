3018060bb439df5b31471595e7c80e0e753e616b
macotosama <jtn1999@outlook.com>
2020-12-12 13:34:31 -0600

    Update

--- ArquiPry3Frontend/src/app/app.component.ts
+++ ArquiPry3Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component'
 
 @Component({
   selector: 'app-root',
--- ArquiPry3Frontend/src/app/app.module.ts
+++ ArquiPry3Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    BarraNavegacionComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
