6b1c2a111caca5434acc08c17ad331949e16305d
macotosama <jtn1999@outlook.com>
2020-12-13 12:43:58 -0600

    Update

--- ArquiPry3Frontend/src/app/app.module.ts
+++ ArquiPry3Frontend/src/app/app.module.ts
@@ -14,11 +14,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { RegistrarComponent } from './interfaz/registrar/registrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainhomeComponent
+    MainhomeComponent,
+    RegistrarComponent
     ],
   imports: [
     BrowserModule,
