c2de05229331e67690e4261d9b965a2ac17e157d
macotosama <jtn1999@outlook.com>
2020-12-15 13:36:59 -0600

    Update

--- ArquiPry3Frontend/src/app/app-routing.module.ts
+++ ArquiPry3Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainhomeComponent } from './interfaz/mainhome/mainhome.component';
+import { MenuComponent } from './interfaz/menu/menu.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: MainhomeComponent },
+  {path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
